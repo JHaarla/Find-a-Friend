2eb5d392d663439c88adefc28e59433b955e78c0
JHaarla <haarladesign@gmail.com>
2019-10-19 00:45:25 -0400

    re-creating Friend Finder

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+const friendsData = require("../data/friends.js");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+       return res.json(friendsData);
+    
+    });
+
+    app.post("/api/friends", function (req, res) {
+        // data collection, comparing and display of match logic goes in here
+        // obviously, this is as far as I have made it. Will continue to work on this ASAP
+
+        //is this even the right place for the app logic???
+    })
+
+};
